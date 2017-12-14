29c1416351792ea720ad312266b413a9ee0b8059
o0white0o <aldrozdovskij@gmail.com>
2017-12-14 03:44:42 +0300

    Final version 14.12.2017

--- LR4/Optimization/LR4/LR4/_Block.cpp
+++ LR4/Optimization/LR4/LR4/_Block.cpp
@@ -2,7 +2,7 @@
 #include "_Block.h"
 
 
-_Block::_Block(int i_old=0, int j_old=0, int i_new=0, int j_new=0, int SAD=9999, int SSD=9999)
+_Block::_Block(int i_old=0, int j_old=0, int i_new=0, int j_new=0, double SAD=9999, int SSD=9999)
 {
 	this->i_old = i_old;
 	this->j_old = j_old;
@@ -17,15 +17,15 @@ _Block::_Block(int i_old, int j_old)
 	this->j_old = j_old;
 	this->SAD = 99999;
 	this->SSD = 99999;
-	this->i_new = 0;
-	this->j_new = 0;
+	this->i_new = i_old;
+	this->j_new = j_old;
 }
 
 
 _Block::~_Block()
 {
 }
-void _Block::reload(int i_new, int j_new, int SAD, int SSD)
+void _Block::reload(int i_new, int j_new, double SAD, int SSD)
 {
 	this->SAD = SAD;
 	this->SSD = SSD;
@@ -33,7 +33,7 @@ void _Block::reload(int i_new, int j_new, int SAD, int SSD)
 	this->j_new = j_new;
 }
 
-int _Block::ret_SAD()
+double _Block::getSAD()
 {
 	return this->SAD;
 }
@@ -42,19 +42,19 @@ int _Block::ret_SSD()
 {
 	return this->SSD;
 }
-int _Block::ret_i_old()
+int _Block::get_i_old()
 {
 	return this->i_old;
 }
-int _Block::ret_j_old()
+int _Block::get_j_old()
 {
 	return this->j_old;
 }
-int _Block::ret_i_new()
+int _Block::get_i_new()
 {
 	return this->i_new;
 }
-int _Block::ret_j_new()
+int _Block::get_j_new()
 {
 	return this->j_new;
 }
--- LR4/Optimization/LR4/LR4/_Block.h
+++ LR4/Optimization/LR4/LR4/_Block.h
@@ -3,18 +3,18 @@ class _Block
 {
 	int i_old, j_old;
 	int i_new, j_new;
-	int SAD;
+	double SAD;
 	int SSD;
 public:
-	_Block(int i_old, int j_old, int i_new, int j_new, int SAD, int SSD);
+	_Block(int i_old, int j_old, int i_new, int j_new, double SAD, int SSD);
 	_Block(int i_old, int j_old);
 	~_Block();
-	void reload(int i_new, int j_new, int SAD, int SSD);
-	int ret_SAD();
+	void reload(int i_new, int j_new, double SAD, int SSD);
+	double getSAD();
 	int ret_SSD();
-	int ret_i_old();
-	int ret_j_old();
-	int ret_i_new();
-	int ret_j_new();
+	int get_i_old();
+	int get_j_old();
+	int get_i_new();
+	int get_j_new();
 };
 
